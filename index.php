eaba0f43a94ca653bc7b4a2988e0f54311725a51
Jo Dickson <jogreybill@gmail.com>
2014-03-18 13:52:10 -0400

    Added google analytics

--- index.php
+++ index.php
@@ -19,6 +19,17 @@ $projects = $content->projects;
 		<link rel="stylesheet" type="text/css" href="fonts/tex-gyre-schola-italic/font.css" />
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Varela" />
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-28550607-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</head>
 	<body>
 		<header>
