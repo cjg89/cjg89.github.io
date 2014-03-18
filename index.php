cba97f47327808419235b0c57fcfe157128eb9ef
Jo Dickson <jogreybill@gmail.com>
2014-03-18 14:03:37 -0400

    Shiny new tracking code

--- index.php
+++ index.php
@@ -19,16 +19,15 @@ $projects = $content->projects;
 		<link rel="stylesheet" type="text/css" href="fonts/tex-gyre-schola-italic/font.css" />
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Varela" />
 		<link href="css/style.css" rel="stylesheet" type="text/css" />
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-28550607-1']);
-		  _gaq.push(['_trackPageview']);
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+		  ga('create', 'UA-28550607-1', 'jodickson.com');
+		  ga('send', 'pageview');
+
 		</script>
 	</head>
 	<body>
